0225cd1877d11c841b90a2bb4b57fdbb5a4c18b3
Piyush Jadhav <pjadhav@ucsb.edu>
2025-06-22 14:23:14 -0700

    added more files to frontend setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import { ClerkProviderWithRoutes } from "./auth/ClerkProviderWithRoutes";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
-  return <> </>
+  return (
+    <ClerkProviderWithRoutes>
+      <Routes></Routes>
+    </ClerkProviderWithRoutes>
+  );
 }
 
-export default App
+export default App;
